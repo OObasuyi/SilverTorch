6fcd4da26237f83c6e5134d97580ded86f11be27
oobasuyi <80005871+OObasuyi@users.noreply.github.com>
2022-04-27 22:25:40 +0200

    added extra

--- terminal_entry.py
+++ terminal_entry.py
@@ -11,7 +11,7 @@ def terminal_entry():
     mandatory_args.add_argument('-ppsm_location',required=True,type=str)
     mandatory_args.add_argument('-access_policy',required=True,type=str)
     mandatory_args.add_argument('-rule_prepend_name',required=True,type=str)
-    mandatory_args.add_argument('-zone_of_last_resort',required=True,type=str)
+    mandatory_args.add_argument('-zolr',help='zone of last resort', required=True,type=str)
 
     optional_args = parser.add_argument_group(title='Optional Args')
     optional_args.add_argument('--domain',default='Global',action="store",type=str)
@@ -24,7 +24,7 @@ def terminal_entry():
 
     fm = AugmentedWorker(cred_file=args.cred_file, ppsm_location=args.ppsm_location ,access_policy=args.access_policy,
                          rule_prepend_name=args.rule_prepend_name,fmc_host=args.fmc_host,ftd_host=args.ftd_host,domain=args.domain,zbr_bypass=args.zbr_bypass,
-                         zone_of_last_resort=args.zone_of_last_resort)
+                         zone_of_last_resort=args.zolr)
     fm.driver()
 
 
